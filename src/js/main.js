6cd129ed8d80b3de343d39957c03846c4e061838
snvoke <snvoke@gmail.com>
2017-06-12 22:08:38 +0400

    дополнение

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,3 @@ const solution = n => getTriangleArea(n, square(n) / 2);
 console.log(solution(0));
 console.log(solution(10));
 console.log(solution(12));
-console.log(solution(14));
-console.log(solution(16));
-console.log(solution(20));
